720da63cfc262502afdb0b76062f2d4c576f0ff7
anjaliGhoghari <anjalighoghari79@gmail.com>
2025-08-22 15:45:53 +0530

    feat:SEO for all-1.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Footer from './components/Footer'
 
 import HeroSection from './components/HeroSection'
 import Project from './components/Project'
-import SEO from './components/SEO'
 import Skills from './components/Skills'
 
 
